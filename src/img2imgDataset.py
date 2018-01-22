2192e0268b3ba934de2e47845fa9ff495a5c0df3
misogil0116 <lack_un@yahoo.co.jp>
2018-01-22 17:47:03 +0900

    thining 420 -> 444

--- src/img2imgDataset.py
+++ src/img2imgDataset.py
@@ -27,7 +27,7 @@ class Image2ImageDataset(object):
         this function is used for training scripts.
         other functions is used for making dataset.
 
-        WATCH: After guetzli, images is transformed into YUV420, so you can think label data Y:Cb:Cr = 2:1:1
+        WATCH: After guetzli, images is transformed into YUV420, so you can think label data Y:Cb:Cr = 4:1:1
         """
         pass
     
@@ -42,11 +42,11 @@ class Image2ImageDataset(object):
     def make_images_and_labels(self):
         qopt_files = os.listdir(self.qopt_path)
         # images and labels
-        # TODO: imread => YCbCrで保存しておく
         images = [cv2.imread(self.qopt_path + "/" + q_file) for q_file in qopt_files if q_file != ".DS_Store"]
         labels = self.dct_csv2numpy_probability()
 
         # TODO: coeff_y, coeff_cb, coeff_crを画像と同じ形にする
+        # TODO: GuetzliはYUV444も採用する可能性があるので次元数とshapeを確認する。
         for img, label in zip(images, labels):
             height = img.shape[0]
             width = img.shape[1]
@@ -57,20 +57,21 @@ class Image2ImageDataset(object):
             seq = int(label.shape[0] * (2/3))
             coeff_y, coeff_cbcr = label[:seq], label[seq:]
             coeff_y = self.resize_coeff_to_img_matrix(coeff_y, width, height)
-            import ipdb; ipdb.set_trace()
             coeff_cb = coeff_cbcr[:int(seq/4)]
             coeff_cr = coeff_cbcr[int(seq/4):]
-    
+            coeff_cb = self.resize420to444(coeff_cb, width, height)
+            coeff_cr = self.resize420to444(coeff_cr, width, height)
+
+
+
     @staticmethod
     def resize_coeff_to_img_matrix(coeff, width, height):
         canvas = np.zeros((height, width))
         width_blocks = int(width / 8)
         height_blocks = int(height / 8)
-        num_blocks = width_blocks * height_blocks
         for block_y in range(height_blocks):
             for block_x in range(width_blocks):
                 block_ix = height_blocks * block_y + block_x
-                
                 block = coeff[block_ix].reshape(8, 8)
                 canvas[block_x*8:block_x*8+8, block_y*8:block_y*8+8] = block
         return canvas
@@ -82,9 +83,23 @@ class Image2ImageDataset(object):
         else:
             return 0
     
+    # TODO: 間引きさせる関数の作成
     @staticmethod
-    def resize422to444(coeff_c):
-        return [coeff_c, coeff_c]
+    def resize420to444(coeff, width, height):
+        canvas = np.zeros((height, width))
+        width_blocks = int(width / 8)
+        height_blocks = int(height / 8)
+        for block_y in range(width_blocks):
+            for block_x in range(height_blocks):
+                canvas16 = np.zeros((16, 16)).astype(np.int32)
+                block_ix = height_blocks * block_y + block_x
+                block = coeff[block_ix].reshape(8, 8)
+                for i in range(8):
+                    for j in range(8):
+                        dct22 = block[i][j] * np.ones((2, 2)).astype(np.int32)
+                        canvas16[j*2:j*2+2, i*2:i*2+2] = dct22
+                canvas[block_x*16:block_x*16+16, block_y*16:block_y*16+16] = canvas16
+        return canvas
 
 
 if __name__ == "__main__":

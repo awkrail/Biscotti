7f2244b36b030bfa3009caa8ef612bf230d9f57a
misogil0116 <lack_un@yahoo.co.jp>
2018-01-22 12:42:32 +0900

    fix img2imgDataset.py

--- dump_qopt_images.sh
+++
@@ -1,8 +0,0 @@
-python resize.py
-
-for file in `cd images/ && ls`; do
-    bin/Release/guetzli resized_images/$file opt_images/$file qopt_images/$file
-    echo "$file done"
-done
-
-python img2imgDataset.py
--- src/img2imgDataset.py
+++ src/img2imgDataset.py
@@ -27,34 +27,53 @@ class Image2ImageDataset(object):
         this function is used for training scripts.
         other functions is used for making dataset.
 
-        WATCH: After guetzli, images is transformed into YUV422, so you can think label data Y:Cb:Cr = 2:1:1
+        WATCH: After guetzli, images is transformed into YUV420, so you can think label data Y:Cb:Cr = 2:1:1
         """
         pass
     
     def dct_csv2numpy_probability(self):
         checker_0 = np.vectorize(self.check0)
         for csv_file in os.listdir(self.csv_path):
+            if csv_file.startswith("."):
+                continue
             csv_numpy = pd.read_csv(self.csv_path + "/" + csv_file, header=None).get_values()
-            binary = checker_0(csv_numpy == 0)
-            yield binary
+            yield checker_0(csv_numpy == 0)
     
     def make_images_and_labels(self):
         qopt_files = os.listdir(self.qopt_path)
         # images and labels
         # TODO: imread => YCbCrで保存しておく
-        images = [cv2.imread(self.qopt_path + "/" + q_file) for q_file in qopt_files]
+        images = [cv2.imread(self.qopt_path + "/" + q_file) for q_file in qopt_files if q_file != ".DS_Store"]
         labels = self.dct_csv2numpy_probability()
 
         # TODO: coeff_y, coeff_cb, coeff_crを画像と同じ形にする
         for img, label in zip(images, labels):
-            seq = int(label.shape[0]/2) 
+            height = img.shape[0]
+            width = img.shape[1]
+
+            height_blocks = int(height / 8)
+            width_blocks = int(width / 8)
+
+            seq = int(label.shape[0] * (2/3))
             coeff_y, coeff_cbcr = label[:seq], label[seq:]
-            coeff_cb = np.array(list(map(self.resize422to444, coeff_cbcr[:int(seq/2)].tolist())))
-            coeff_cr = np.array(list(map(self.resize422to444, coeff_cbcr[int(seq/2):].tolist())))
+            coeff_y = self.resize_coeff_to_img_matrix(coeff_y, width, height)
             import ipdb; ipdb.set_trace()
-
-
-
+            coeff_cb = coeff_cbcr[:int(seq/4)]
+            coeff_cr = coeff_cbcr[int(seq/4):]
+    
+    @staticmethod
+    def resize_coeff_to_img_matrix(coeff, width, height):
+        canvas = np.zeros((height, width))
+        width_blocks = int(width / 8)
+        height_blocks = int(height / 8)
+        num_blocks = width_blocks * height_blocks
+        for block_y in range(height_blocks):
+            for block_x in range(width_blocks):
+                block_ix = height_blocks * block_y + block_x
+                
+                block = coeff[block_ix].reshape(8, 8)
+                canvas[block_x*8:block_x*8+8, block_y*8:block_y*8+8] = block
+        return canvas
 
     @staticmethod
     def check0(coeff):

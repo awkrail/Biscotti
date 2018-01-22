52b97b81b5f57a178d481585220fff75e56f9059
misogil0116 <lack_un@yahoo.co.jp>
2018-01-22 19:21:57 +0900

    fix img2imgDataset.py

--- bin/script/dump_qopt_images.sh
+++ bin/script/dump_qopt_images.sh
@@ -1,8 +1,6 @@
-python resize.py
-
+python src/resize.py
 for file in `cd images/ && ls`; do
     bin/Release/guetzli resized_images/$file opt_images/$file qopt_images/$file
     echo "$file done"
 done
-
-python img2imgDataset.py
+python src/img2imgDataset.py
--- src/img2imgDataset.py
+++ src/img2imgDataset.py
@@ -19,7 +19,7 @@ labels/ => read csv(DCT coeffs), and if each coeff is not 0, set a value 1. if e
 class Image2ImageDataset(object):
     def __init__(self):
         self.qopt_path = "qopt_images/"
-        self.label_path = "label/"
+        self.train_path = "train/"
         self.csv_path = "csv/"
     
     def load_dataset(self):
@@ -27,9 +27,13 @@ class Image2ImageDataset(object):
         this function is used for training scripts.
         other functions is used for making dataset.
 
-        WATCH: After guetzli, images is transformed into YUV420, so you can think label data Y:Cb:Cr = 4:1:1
+        WATCH: After guetzli, images is transformed into YUV420 or YUV444, so you can think label data Y:Cb:Cr = 4:1:1
+        TODO: this script doesn't accept YUV444 images.
+        TODO: this script doesn't accept png file.
+        TODO: this script doesn't accept gray scale.. so I diciced to use PIL Image, not cv2.
         """
         pass
+
     
     def dct_csv2numpy_probability(self):
         checker_0 = np.vectorize(self.check0)
@@ -41,28 +45,36 @@ class Image2ImageDataset(object):
     
     def make_images_and_labels(self):
         qopt_files = os.listdir(self.qopt_path)
-        # images and labels
-        images = [cv2.imread(self.qopt_path + "/" + q_file) for q_file in qopt_files if q_file != ".DS_Store"]
+        images = [cv2.imread(self.qopt_path + "/" + q_file) for q_file in qopt_files if not q_file.startswith(".")]
         labels = self.dct_csv2numpy_probability()
-
-        # TODO: coeff_y, coeff_cb, coeff_crを画像と同じ形にする
         # TODO: GuetzliはYUV444も採用する可能性があるので次元数とshapeを確認する。
-        for img, label in zip(images, labels):
+        for i in range(len(qopt_files)):
+            img = images[i]
+            label = next(labels)
+            filename = qopt_files[i].replace(".jpg", "").replace(".jpeg", "").replace(".png", "")
+            print(filename)
+
             height = img.shape[0]
             width = img.shape[1]
-
             height_blocks = int(height / 8)
             width_blocks = int(width / 8)
-
             seq = int(label.shape[0] * (2/3))
+
+            print("==== shape ====")
+            print(height)
+            print(width)
+            print(height_blocks)
+            print(width_blocks)
+            print(label.shape[0])
+            print(label.shape[1])
+            print("===============")
             coeff_y, coeff_cbcr = label[:seq], label[seq:]
             coeff_y = self.resize_coeff_to_img_matrix(coeff_y, width, height)
-            coeff_cb = coeff_cbcr[:int(seq/4)]
-            coeff_cr = coeff_cbcr[int(seq/4):]
-            coeff_cb = self.resize420to444(coeff_cb, width, height)
-            coeff_cr = self.resize420to444(coeff_cr, width, height)
-
-
+            coeff_cb = self.resize420to444(coeff_cbcr[:int(seq/4)], width, height)
+            coeff_cr = self.resize420to444(coeff_cbcr[int(seq/4):], width, height)
+            coeff3d = np.concatenate((coeff_y, coeff_cb, coeff_cr), axis=2)
+            result = np.concatenate((img, coeff3d), axis=2)
+            np.save(self.train_path + filename + ".npy", result)
 
     @staticmethod
     def resize_coeff_to_img_matrix(coeff, width, height):
@@ -72,9 +84,10 @@ class Image2ImageDataset(object):
         for block_y in range(height_blocks):
             for block_x in range(width_blocks):
                 block_ix = height_blocks * block_y + block_x
+                # print(block_ix)
                 block = coeff[block_ix].reshape(8, 8)
                 canvas[block_x*8:block_x*8+8, block_y*8:block_y*8+8] = block
-        return canvas
+        return canvas.reshape(height, width, 1)
 
     @staticmethod
     def check0(coeff):
@@ -87,8 +100,8 @@ class Image2ImageDataset(object):
     @staticmethod
     def resize420to444(coeff, width, height):
         canvas = np.zeros((height, width))
-        width_blocks = int(width / 8)
-        height_blocks = int(height / 8)
+        width_blocks = int(width / 16)
+        height_blocks = int(height / 16)
         for block_y in range(width_blocks):
             for block_x in range(height_blocks):
                 canvas16 = np.zeros((16, 16)).astype(np.int32)
@@ -99,8 +112,7 @@ class Image2ImageDataset(object):
                         dct22 = block[i][j] * np.ones((2, 2)).astype(np.int32)
                         canvas16[j*2:j*2+2, i*2:i*2+2] = dct22
                 canvas[block_x*16:block_x*16+16, block_y*16:block_y*16+16] = canvas16
-        return canvas
-
+        return canvas.reshape(height, width, 1)
 
 if __name__ == "__main__":
     dataset = Image2ImageDataset()

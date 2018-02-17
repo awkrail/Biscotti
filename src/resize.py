4159c9665ee5b1cb4344090b77734eff2468114f
misogil0116 <lack_un@yahoo.co.jp>
2018-02-17 20:55:54 +0900

    change code src/resize.py

--- src/resize.py
+++ src/resize.py
@@ -7,6 +7,38 @@ this script changes images size to the DCT table size.
 """
 
 
+class ImageRisizer(object):
+    def __init__(self, image_files):
+        self.image_files = sorted(image_files)
+    
+    def resize(self, predict=False):
+        images = [cv2.imread("images/" + images_file) for images_file in self.images_files]
+        for image, images_file in zip(image, self.images_files):
+            if images_file.startswith("."):
+                continue
+            if predict:
+                row = image.shape[0]
+                col = image.shape[1]
+                mod_r_8 = row % 8
+                mod_c_8 = col % 8
+                r_padding = 0
+                c_padding = 0
+
+                if mod_r_8 != 0:
+                    r_padding += (8 - mod_r_8)
+                
+                if mod_c_8 != 0:
+                    c_padding += (8 - mod_c_8)
+                
+                new_row = row + mod_r_8
+                new_col = col + mod_c_8
+                resized_image = cv2.resize(image, (new_row, new_col), interpolation=cv2.INTER_NEAREST)
+                cv2.imwrite("resized_images/" + images_file, resized_image)
+            else:
+                resized_image = cv2.resize(image, (224, 224))
+                cv2.imwrite("resized_images/" + images_file, resized_image)
+
+
 def change_image_size_to_dct(image):
     row = image.shape[0]
     col = image.shape[1]
@@ -22,7 +54,8 @@ def change_image_size_to_dct(image):
         
     if mod_c_8 != 0:
         c_padding += (8 - mod_c_8)
-        
+    
+
     foundation = np.zeros((row+r_padding, col+c_padding, 3))
     foundation[:image.shape[0], :image.shape[1], :image.shape[2]] = image
     print("before row, col (", row, ", ", col, " )")
@@ -31,10 +64,7 @@ def change_image_size_to_dct(image):
 
 if __name__ == "__main__":
     print("=== resizing dataset ... ===")
-    images_files = os.listdir("images/")
-    images = [cv2.imread("images/" + image_file) for image_file in images_files]
-    
-    for pad_image, image_file in zip(map(change_image_size_to_dct, images), images_files):
-        print(image_file ," done!")
-        cv2.imwrite("resized_images/" + image_file, pad_image)
+    image_files = os.listdir("images/")
+    resizer = ImageRisizer(image_files)
+    resizer.resize()
     print("===> Done!")

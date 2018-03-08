220120d54a93f9b063fc0f653878f90b7d7600b4
misogil0116 <lack_un@yahoo.co.jp>
2018-03-08 16:35:53 +0900

    add argparse

--- src/predict.py
+++ src/predict.py
@@ -1,3 +1,5 @@
+import argparse
+
 import numpy as np
 import pandas as pd
 import cv2
@@ -5,7 +7,7 @@ from keras import backend as K
 from keras.callbacks import ModelCheckpoint
 from keras.models import Model
 from keras.layers import Input
-from keras.layers import Conv2D, MaxPooling2D, UpSampling2D
+from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, LeakyReLU, BatchNormalization, Activation
 # from keras.layers import merge
 from keras.layers import concatenate
 from keras.optimizers import Adam
@@ -14,16 +16,17 @@ import matplotlib.pyplot as plt
 
 
 class DctCsvLoader(object):
-    def __init__(self, guetzli_csv_path):
+    def __init__(self, guetzli_csv_path, target_size):
         self.csv_path = guetzli_csv_path
         self.dct = pd.read_csv(guetzli_csv_path, header=None).get_values()
+        self.target_size = target_size
     
     def get_csv(self):
         label = self.dct_csv2numpy_probability()
         seq = int(label.shape[0] * (2/3))
         # TODO: I need to adjust these variable values for input image size
-        width = 224
-        height = 224
+        width = self.target_size[1]
+        height = self.target_size[0]
 
         coeff_y, coeff_cbcr = label[:seq], label[seq:]
         coeff_y = self.resize_coeff_to_img_matrix(coeff_y, width, height)
@@ -81,9 +84,9 @@ class Predictor():
         self.threshold = threshold
         self.result_png_path = result_png_path
         self.csv_path = csv_path
-        self.model = self.create_fcn()
+        self.model = self.create_generator()
         self.model.load_weights(model_path)
-        self.guetzli_dct = DctCsvLoader(guetzli_csv_path).get_csv()
+        self.guetzli_dct = DctCsvLoader(guetzli_csv_path, target_size).get_csv()
         self.predict_dct = None
     
     def predict(self):
@@ -141,59 +144,59 @@ class Predictor():
         y_df.to_csv(self.csv_path + "y.csv", header=None, index=None)
         cr_df.to_csv(self.csv_path + "cr.csv", header=None, index=None)
         cb_df.to_csv(self.csv_path + "cb.csv", header=None, index=None)
-    
-    def create_fcn(self):
+
+    def create_generator(self):
+        # U-Net
+        # EncoderSide
         inputs = Input((self.target_size[0], self.target_size[1], 3))
-        conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format="channels_last")(inputs)
-        conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format="channels_last")(conv1)
-        pool1 = MaxPooling2D(pool_size=(2, 2), data_format="channels_last")(conv1)
 
-        conv2 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format="channels_last")(pool1)
-        conv2 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format="channels_last")(conv2)
-        pool2 = MaxPooling2D(pool_size=(2, 2), data_format="channels_last")(conv2)
+        conv1 = Conv2D(32, (3, 3), padding='same', data_format="channels_last")(inputs)
+        conv1 = LeakyReLU(0.2)(conv1)
 
-        conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', data_format="channels_last")(pool2)
-        conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', data_format="channels_last")(conv3)
-        pool3 = MaxPooling2D(pool_size=(2, 2), data_format="channels_last")(conv3)
+        conv2 = Conv2D(64, (3, 3), strides=(2, 2), padding='same', data_format="channels_last")(conv1)
+        conv2 = LeakyReLU(0.2)(conv2)
+        conv2 = BatchNormalization(axis=-1)(conv2)
 
-        conv4 = Conv2D(256, (3, 3), activation='relu', padding='same', data_format="channels_last")(pool3)
-        conv4 = Conv2D(256, (3, 3), activation='relu', padding='same', data_format="channels_last")(conv4)
-        pool4 = MaxPooling2D(pool_size=(2, 2), data_format="channels_last")(conv4)
+        conv3 = Conv2D(128, (3, 3), strides=(2, 2), padding='same', data_format="channels_last")(conv2)
+        conv3 = LeakyReLU(0.2)(conv3)
+        conv3 = BatchNormalization(axis=-1)(conv3)
 
-        conv5 = Conv2D(512, (3, 3), activation='relu', padding='same', data_format="channels_last")(pool4)
-        conv5 = Conv2D(512, (3, 3), activation='relu', padding='same', data_format="channels_last")(conv5)
-        pool5 = MaxPooling2D(pool_size=(2, 2), data_format="channels_last")(conv5) # 元は(2, 2)
+        conv4 = Conv2D(256, (3, 3), strides=(2, 2), padding='same', data_format="channels_last")(conv3)
+        conv4 = LeakyReLU(0.2)(conv4)
+        conv4 = BatchNormalization(axis=-1)(conv4)
 
-        conv6 = Conv2D(1024, (3, 3), activation='relu', padding='same', data_format="channels_last")(pool5)
-        conv6 = Conv2D(1024, (3, 3), activation='relu', padding='same', data_format="channels_last")(conv6)
+        conv5 = Conv2D(512, (3, 3), strides=(2, 2), padding='same', data_format="channels_last")(conv4)
+        conv5 = LeakyReLU(0.2)(conv5)
+        conv5 = BatchNormalization(axis=-1)(conv5)
 
-        # up7 = merge([UpSampling2D(size=(2, 2))(conv6), conv5], mode='concat', concat_axis=1)
-        up7 = concatenate([UpSampling2D(size=(2, 2), data_format="channels_last")(conv6), conv5], axis=3) # 元は(2, 2)
-        conv7 = Conv2D(512, (3, 3), activation='relu', padding='same', data_format="channels_last")(up7)
-        conv7 = Conv2D(512, (3, 3), activation='relu', padding='same', data_format="channels_last")(conv7)
+        conv6 = Conv2D(1024, (3, 3), strides=(2, 2), padding='same', data_format="channels_last")(conv5)
+        conv6 = Conv2D(1024, (3, 3), padding='same', data_format="channels_last")(conv6)
+        conv6 = Activation('relu')(conv6)
 
-        # up8 = merge([UpSampling2D(size=(2, 2))(conv7), conv4], mode='concat', concat_axis=1)
-        up8 = concatenate([UpSampling2D(size=(2, 2), data_format="channels_last")(conv7), conv4], axis=3)
-        conv8 = Conv2D(256, (3, 3), activation='relu', padding='same', data_format="channels_last")(up8)
-        conv8 = Conv2D(256, (3, 3), activation='relu', padding='same', data_format="channels_last")(conv8)
+        # Decoder Side
+        up1 = concatenate([UpSampling2D(size=(2, 2), data_format="channels_last")(conv6), conv5], axis=3) # 元は(2, 2)
+        conv7 = Conv2D(512, (3, 3), activation='relu', padding='same', data_format="channels_last")(up1)
+        conv7 = BatchNormalization(axis=-1)(conv7)
 
-        # up9 = merge([UpSampling2D(size=(2, 2))(conv8), conv3], mode='concat', concat_axis=1)
-        up9 = concatenate([UpSampling2D(size=(2, 2), data_format="channels_last")(conv8), conv3], axis=3)
-        conv9 = Conv2D(128, (3, 3), activation='relu', padding='same', data_format="channels_last")(up9)
-        conv9 = Conv2D(128, (3, 3), activation='relu', padding='same', data_format="channels_last")(conv9)
+        up2 = concatenate([UpSampling2D(size=(2, 2), data_format="channels_last")(conv7), conv4], axis=3)
+        conv8 = Conv2D(256, (3, 3), activation='relu', padding='same', data_format="channels_last")(up2)
+        conv8 = BatchNormalization(axis=-1)(conv8)
 
-        # up10 = merge([UpSampling2D(size=(2, 2))(conv9), conv2], mode='concat', concat_axis=1)
-        up10 = concatenate([UpSampling2D(size=(2, 2), data_format="channels_last")(conv9), conv2], axis=3)
-        conv10 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format="channels_last")(up10)
-        conv10 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format="channels_last")(conv10)
+        up3 = concatenate([UpSampling2D(size=(2, 2), data_format="channels_last")(conv8), conv3], axis=3)
+        conv9 = Conv2D(128, (3, 3), activation='relu', padding='same', data_format="channels_last")(up3)
+        conv9 = BatchNormalization(axis=-1)(conv9)
 
-        # up11 = merge([UpSampling2D(size=(2, 2))(conv10), conv1], mode='concat', concat_axis=1)
-        up11 = concatenate([UpSampling2D(size=(2, 2), data_format="channels_last")(conv10), conv1], axis=3)
-        conv11 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format="channels_last")(up11)
-        conv11 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format="channels_last")(conv11)
+        up4 = concatenate([UpSampling2D(size=(2, 2), data_format="channels_last")(conv9), conv2], axis=3)
+        conv10 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format="channels_last")(up4)
+        conv10 = BatchNormalization(axis=-1)(conv10)
+
+        up5 = concatenate([UpSampling2D(size=(2, 2), data_format="channels_last")(conv10), conv1], axis=3)
+        conv11 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format="channels_last")(up5)
+        conv11 = BatchNormalization(axis=-1)(conv11)
 
         conv12 = Conv2D(3, (1, 1), activation='sigmoid', data_format="channels_last")(conv11)
         fcn = Model(input=inputs, output=conv12)
+
         return fcn
     
     @staticmethod
@@ -249,15 +252,31 @@ class Predictor():
 
 
 if __name__ == "__main__":
-    target_size = (224, 224) # change to your image size
+    parser = argparse.ArgumentParser(description="predict dataset")
+    parser.add_argument("--modelpath", "-m", type=str, 
+                        required=True, help="load trained model weights")
+    parser.add_argument("--imagepath", "-i", type=str, 
+                        required=True, help="load Input Image")
+    parser.add_argument("--targetsize", "-t", type=int,
+                        default=224)
+    parser.add_argument("--resultpath", "-r", type=str, default="test/heatmap/",
+                        help="save output in this result path")
+    parser.add_argument("--csvpath", "-c", type=str, default="test/coeffs_csv/",
+                        help="default csv path")
+    parser.add_argument("--guetzli_csv_path", "-gc", type=str, 
+                        help="guetzli csv path")
+    args = parser.parse_args()
+
+
+    target_size = (args.targetsize, args.targetsize) # change to your image size
     # model_path, result_png_path, csv_path, guetzli_csv_path
-    image = cv2.imread("test/illust.jpg")
+    image = cv2.imread(args.imagepath)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb) / 255.0
     predictor = Predictor(target_size, image, threshold=0.5,
-                    model_path="checkpoints/model_weights_151_ver2.h5", 
-                    result_png_path="test/heatmap/",
-                    csv_path="test/coeffs_csv/",
-                    guetzli_csv_path="csv/illust.csv")
+                    model_path=args.modelpath, 
+                    result_png_path=args.resultpath,
+                    csv_path=args.csvpath,
+                    guetzli_csv_path=args.guetzli_csv_path)
     predictor.predict()
     predictor.eval()
     predictor.plot(plot_guetzli_dct=True, binary=True)

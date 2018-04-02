ef69723ad86966d87562488cddbf98612e3b8d76
misogil0116 <lack_un@yahoo.co.jp>
2018-04-02 10:52:03 +0900

    add nets.py 3layer unet

--- src/nets.py
+++ src/nets.py
@@ -53,6 +53,42 @@ def discriminator(img_shape, disc_img_shape, patch_num):
     discriminator_model = Model(inputs=(list_input+list_raw_input), outputs=[x_out], name="Discriminator")
     return discriminator_model
 
+def generator_3layer(input_size):
+    inputs = Input((input_size[0], input_size[1], 3))
+    conv1 = Conv2D(32, (3, 3), padding='same')(inputs)
+    conv1 = LeakyReLU(0.2)(conv1)
+
+    conv2 = Conv2D(64, (3, 3), strides=(2, 2), padding='same')(conv1)
+    conv2 = LeakyReLU(0.2)(conv2)
+    conv2 = BatchNormalization(axis=-1)(conv2)
+
+    conv3 = Conv2D(128, (3, 3), strides=(2, 2), padding='same')(conv2)
+    conv3 = LeakyReLU(0.2)(conv3)
+    conv3 = BatchNormalization(axis=-1)(conv3)
+
+    conv4 = Conv2D(256, (3, 3), strides=(2, 2), padding='same')(conv3)
+    conv4 = Conv2D(256, (3, 3), padding="same")(conv4)
+    conv4 = Activation('relu')(conv4)
+
+    # Decoder Side
+    up1 = concatenate([UpSampling2D(size=(2, 2), data_format="channels_last")(conv4), conv3], axis=3)
+    conv5 = Conv2D(128, (3, 3), activation='relu', padding='same')(up1)
+    conv5 = BatchNormalization(axis=-1)(conv5)
+
+    up2 = concatenate([UpSampling2D(size=(2, 2), data_format="channels_last")(conv5), conv2], axis=3)
+    conv6 = Conv2D(64, (3, 3), activation='relu', padding='same')(up2)
+    conv6 = BatchNormalization(axis=-1)(conv6)
+    
+    up3 = concatenate([UpSampling2D(sie=(2, 2), data_format="channels_last")(conv6), conv1], axis=-1)
+    conv7 = Conv2D(32, (3, 3), activation='relu', padding='same')(up3)
+    conv7 = BatchNormalization(axis=-1)(conv7)
+
+    conv8 = Conv2D(3, (1, 1), activation='sigmoid', data_format="channels_last")(conv7)
+    fcn = Model(input=inputs, output=conv8)
+
+    return fcn
+
+
 # U-Net
 def generator(input_size):
     # EncoderSide
@@ -82,7 +118,7 @@ def generator(input_size):
     conv6 = Activation('relu')(conv6)
 
     # Decoder Side
-    up1 = concatenate([UpSampling2D(size=(2, 2), data_format="channels_last")(conv6), conv5], axis=3) # 元は(2, 2)
+    up1 = concatenate([UpSampling2D(size=(2, 2), data_format="channels_last")(conv6), conv5], axis=3)
     conv7 = Conv2D(512, (3, 3), activation='relu', padding='same', data_format="channels_last")(up1)
     conv7 = BatchNormalization(axis=-1)(conv7)
 

b8f90a3776b81289b64bb56af366d85f85703f2b
misogil0116 <lack_un@yahoo.co.jp>
2018-05-24 15:21:17 +0900

    fix validation_dir

--- src/measure_performance.py
+++ src/measure_performance.py
@@ -1,15 +1,20 @@
 import os
 import argparse
 
-def main():
-  images = os.listdir("validations/images/")
+def main(size, validation_dir):
+  images = os.listdir(validation_dir)
   for image in images:
-    pass
+    # replace image
+    ok = os.system("bin/Release/biscotti" validation_dir + "63893827.jpg " + validation_dir + "/opt_" + "63893827" + ".jpg")
+    import ipdb; ipdb.set_trace()
   
 
 if __name__ == "__main__":
   # 16で割り切れるという条件付き
   # TODO : この条件も本体で実装できたら消す
-  # TODO : GrayScaleはデータとして受け付けないように処理を変える
   parser = argparse.ArgumentParser(description="resize images and measure performance biscotti")
-  main()
+  parser.add_argument("--size", "-s", type=int,
+                      required=True, help="image size")
+  parser.add_argument("--valid_dir", "-v", type=str, help="validation directory")
+  args = parser.parse_args()
+  main(args.size, args.valid_dir)

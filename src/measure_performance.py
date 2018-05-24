bd63e8ed819222b3b4ac0ad06958c807be044059
misogil0116 <lack_un@yahoo.co.jp>
2018-05-24 17:33:13 +0900

    update src/measure_performance.py

--- src/measure_performance.py
+++ src/measure_performance.py
@@ -1,13 +1,32 @@
 import os
+import subprocess
 import argparse
 
 def main(size, validation_dir):
   images = os.listdir(validation_dir)
+  grayscale = 0
+  scores = []
   for image in images:
+    # TODO : 16で両辺が割り切れないとダメ => それへの対応
+    # TODO : 1000枚に実行するので遅い。夜に実行する
     # replace image
-    ok = os.system("bin/Release/biscotti" validation_dir + "63893827.jpg " + validation_dir + "/opt_" + "63893827" + ".jpg")
-    import ipdb; ipdb.set_trace()
-  
+    biscotti = ["bin/Release/biscotti", validation_dir + image, validation_dir + "/opt_" + image]
+    try:
+      subprocess.check_call(biscotti)
+      butteraugli = ["bin/Release/butteraugli", biscotti[1], biscotti[2]]
+      score = subprocess.check_output(butteraugli)
+      score = float(score)
+      scores.append(score)
+    except:
+      grayscale = 0
+
+  print(" --- stats --- ")
+  print("the number of images : ", len(images))
+  print("except for grayscale : ", len(scores))
+  print("grayscale : ", grayscale)
+  print("minimum butteraugli : ", min(scores))
+  print("maximum butteraugli : ", max(scores))
+  print("average butteraugli : ", sum(scores) / len(scores)) 
 
 if __name__ == "__main__":
   # 16で割り切れるという条件付き
@@ -15,6 +34,7 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="resize images and measure performance biscotti")
   parser.add_argument("--size", "-s", type=int,
                       required=True, help="image size")
-  parser.add_argument("--valid_dir", "-v", type=str, help="validation directory")
+  parser.add_argument("--valid_dir", "-v", type=str,
+                      default="validations/images/", help="validation directory")
   args = parser.parse_args()
   main(args.size, args.valid_dir)

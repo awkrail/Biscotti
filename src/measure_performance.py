afabae61a34ca4ab78c672c6f15ad46fe7f3824e
misogil0116 <lack_un@yahoo.co.jp>
2018-05-25 12:16:13 +0900

    fix measure_performance.py

--- src/measure_performance.py
+++ src/measure_performance.py
@@ -1,24 +1,31 @@
 import os
 import subprocess
 import argparse
+from datetime import datetime
 
-def main(size, validation_dir):
+def main(validation_dir, save_dir):
   images = os.listdir(validation_dir)
   grayscale = 0
   scores = []
+  elapsed = []
+  print(len(images))
   for image in images:
     # TODO : 16で両辺が割り切れないとダメ => それへの対応
     # TODO : 1000枚に実行するので遅い。夜に実行する
     # replace image
-    biscotti = ["bin/Release/biscotti", validation_dir + image, validation_dir + "/opt_" + image]
+    biscotti = ["bin/Release/biscotti", validation_dir + "/" + image, save_dir + "/" + image]
     try:
+      start = datetime.now()
       subprocess.check_call(biscotti)
-      butteraugli = ["bin/Release/butteraugli", biscotti[1], biscotti[2]]
+      end = datetime.now()
+      delta = end - start
+      elapsed.append(delta.total_seconds())
+      butteraugli = ["train_bin/Release/butteraugli", biscotti[1], biscotti[2]]
       score = subprocess.check_output(butteraugli)
       score = float(score)
       scores.append(score)
     except:
-      grayscale = 0
+      grayscale += 1
 
   print(" --- stats --- ")
   print("the number of images : ", len(images))
@@ -27,14 +34,15 @@ def main(size, validation_dir):
   print("minimum butteraugli : ", min(scores))
   print("maximum butteraugli : ", max(scores))
   print("average butteraugli : ", sum(scores) / len(scores)) 
+  print("average elapsed time :", sum(elapsed) / len(elapsed))
 
 if __name__ == "__main__":
   # 16で割り切れるという条件付き
   # TODO : この条件も本体で実装できたら消す
   parser = argparse.ArgumentParser(description="resize images and measure performance biscotti")
-  parser.add_argument("--size", "-s", type=int,
-                      required=True, help="image size")
   parser.add_argument("--valid_dir", "-v", type=str,
                       default="validations/images/", help="validation directory")
+  parser.add_argument("--save_dir", "-s", type=str,
+                      required=True, help="save dir")
   args = parser.parse_args()
-  main(args.size, args.valid_dir)
+  main(args.valid_dir, args.save_dir)

0a9df4596384dbb56ae61ae0129c37f5d5a9996b
misogil0116 <lack_un@yahoo.co.jp>
2018-08-22 17:21:43 +0900

    add change_sampling_factor.py

---
+++ src/change_sampling_factor.py
@@ -0,0 +1,26 @@
+# To make dataset, change sampling factor with Imagemagick.
+import argparse
+import os
+import subprocess
+
+def main(args):
+  input_dir = args.input_images
+  sampling_factor = args.sampling_factor
+  output_dir = args.output_images
+
+  for i_path in os.listdir(input_dir):
+    if sampling_factor == 420:
+      command = ["convert", input_dir + "/" + i_path, '-sampling-factor "2x2,1x1,1x1"', output_dir + "/" + i_path]
+    else:
+      command = ["convert", input_dir + "/" + i_path, '-sampling-factor "1x1,1x1,1x1"', output_dir + "/" + i_path]
+    subprocess.check_call(command)
+    print(i_path + " done!")
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Making dataset for training biscotti")
+  parser.add_argument("--input_images", '-i', type=str, required=True) # path of input images
+  parser.add_argument("--sampling_factor", '-s', type=int, required=True) # 420 or 444
+  parser.add_argument("--output_images", '-o', type=str, required=True) # path of output images
+  args = parser.parse_args()
+  main(args)
+  print("Done!")

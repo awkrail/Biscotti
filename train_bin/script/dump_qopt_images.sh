f512fd5f133ba74376b61d61a95424e7266f9eff
misogil0116 <lack_un@yahoo.co.jp>
2018-08-24 10:19:24 +0900

    fix

--- train_bin/script/dump_qopt_images.sh
+++ train_bin/script/dump_qopt_images.sh
@@ -1,6 +1,6 @@
+python src/mkdir.py
 cd guetzli_dumper && make
 cd .. && mv guetzli_dumper/bin/Release/guetzli train_bin/Release/guetzli_dumper
-python src/mkdir.py
 python src/resize.py
 for file in `cd images/ && ls`; do
     train_bin/Release/guetzli_dumper resized_images/$file opt_images/$file qopt_images/$file
---
+++ train_bin/script/measure_performance.sh
@@ -0,0 +1,2 @@
+python src/measure_performance.py -v validations/images512/ -s validations/biscotti_images512/
+python src/measure_performance.py -v validations/images1200/ -s validations/biscotti_images1200/
